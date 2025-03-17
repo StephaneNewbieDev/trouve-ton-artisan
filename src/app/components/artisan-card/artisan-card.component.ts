c1e6c891b67ba9fb9fecc35766d7a1908df72d74
STEPH <s.desray@gmail.com>
2025-03-17 22:57:05 +0100

    add initial commit

---
+++ src/app/components/artisan-card/artisan-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisan-card',
+  imports: [],
+  templateUrl: './artisan-card.component.html',
+  styleUrl: './artisan-card.component.css'
+})
+export class ArtisanCardComponent {
+
+}
